1c1912f1fe13fcc022481873f7aa880534890e55
Carl Winkler <carl@carlossus.com>
2015-12-04 09:53:32 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,120 @@
+const resolve = require('path').resolve;
+const exec = require('child_process').exec;
+const hapi = require('hapi');
+const Promise = require('bluebird');
+const fs = require('fs');
+
+var gitRemote = 'origin';
+var gitBranch = 'master';
+var projectLocation = '';
+var stagingProcess;
+
+var web = new hapi.Server();
+
+function configure(projectPath, options) {
+    options = options || {};
+    var port = options.port || 3000;
+    
+    if (options.remote) gitRemote = options.remote;
+    if (options.branch) gitBranch = options.branch;
+    
+    projectLocation = projectPath;
+    
+    web.connect({ port });
+    web.start(() => console.log('Server listening on port', port));
+    
+    stagingProcess = stage();
+}
+
+web.route({
+    method: 'POST',
+    path: '/payload',
+    handler: (request, reply) => {
+        var event = request.headers['X-Github-Event'] || '';
+        console.log('Received Github Event:', event);
+        if (event !== 'push') return reply('');
+        
+        var branch = request.payload.ref.split('/').slice(-1);
+        
+        if (branch !== gitBranch) {
+            console.log (`Branch pushed was ${branch}, not ${gitBranch}`).
+            console.log('Will not stage');
+            return;
+        }
+        
+        console.log('Updating staging process...');
+        if (stagingProcess) stagingProcess.kill();
+
+        var stagingProcess = stage();
+    }
+});
+
+
+
+function stage() {
+    return pullLatest()
+        .then(() => installDeps())
+        .then(() => startApp());
+}
+
+function installDeps() {
+    console.log('Installing Node deps...')
+    return run('npm install')
+        .then(() => {
+            console.log('Completed');
+            return true;
+        });
+}
+
+function startApp() {
+    var stdout = msg => console.log(msg);
+    var stderr = msg => console.log('ERROR:', stderr);
+    return run('npm start', { stdout, stderr });
+}
+
+function pullLatest() {
+    console.log('Pulling latest code from source...')
+    return run(`git pull ${gitRemote} ${gitBranch}`)
+        .then(() => {
+            console.log('Completed');
+            return true;
+        });
+}
+
+function run(command, options) {
+    options = options || {};
+
+    var child = exec(command, { cwd: projectLocation }, (error, stderr, stdout) => {
+
+    });
+
+    var promise = new Promise((resolve, reject) => {
+
+        child.on('close', rawCode => {
+            var code = Number(rawCode);
+            console.log(command, 'terminated with', code);
+            if (code !== 0) return reject(code);
+            resolve(code);
+        });
+
+        if (typeof options.error === 'function') {
+            child.on('error', options.error);
+        }
+
+        if (typeof options.stdout === 'function') {
+            child.stdout.on('data', options.stdout);
+        }
+
+        if (typeof options.stderr === 'function') {
+            child.stderr.on('data', options.stderr);
+        }
+
+
+    });
+
+    promise.kill = child.kill;
+
+    return promise;
+}
+
+module.exports = configure;
